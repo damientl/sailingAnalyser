56e40c4a46fb2c23d9dcea6fdf6fa5f4b0a9dc15
damientl <damien.tl@gmail.com>
2017-10-12 14:15:13 -0300

    refactor segments

--- web/src/app/track.component.ts
+++ web/src/app/track.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { SegmentService } from './segment.service'
-import { OnInit, ElementRef, ViewChild, Input } from '@angular/core';
+import { OnInit, ElementRef, ViewChild} from '@angular/core';
 import { Segment } from './segment';
 import { TrackWindow } from './track.window';
 import { CanvasPoint } from './canvas.point';
@@ -14,7 +14,7 @@ import { Calculation } from './calculation';
 export class TrackComponent  implements OnInit {
   @ViewChild('myCanvas') canvasRef: ElementRef;
   segments:Segment[];
-  @Input() trackWindow:TrackWindow;
+  trackWindow:TrackWindow;
 
   constructor(private segmentService: SegmentService) {
     this.trackWindow = new TrackWindow();
@@ -33,26 +33,35 @@ export class TrackComponent  implements OnInit {
 
   drawSegments(): void {
     let ctx: CanvasRenderingContext2D =
-    this.canvasRef.nativeElement.getContext('2d');
+      this.canvasRef.nativeElement.getContext('2d');
     ctx.beginPath();
 
     let i = 1;
     for (let seg of this.segments) {
-      let point = new Calculation().pointOnCanvas(new CanvasPoint(seg.lon,seg.lat), this.trackWindow);
-
-      if(i == 1){
-        ctx.moveTo(point.x, point.y);
-        console.log(`move point x: ${ point.x }, point y: ${ point.y }.`);
-      } else {
-        ctx.lineTo(point.x, point.y);
-        console.log(`line point x: ${ point.x }, point y: ${ point.y }.`);
-      }
+      this.drawSegment(seg, ctx, i==1);
       i++;
     }
     ctx.stroke();
   }
+  
+  drawSegment(seg, ctx, first):void{
+    let point = new Calculation().pointOnCanvas(new CanvasPoint(seg.lon,seg.lat), this.trackWindow);
+
+    if(first){
+      ctx.moveTo(point.x, point.y);
+    } else {
+      ctx.lineTo(point.x, point.y);
+    }
+  }
+
+  clearSegments():void {
+    let ctx: CanvasRenderingContext2D =
+    this.canvasRef.nativeElement.getContext('2d');
+    ctx.clearRect(0, 0, this.trackWindow.canvasWidth, this.trackWindow.canvasHeight);
+  }
 
   handleZoomChange():void{
-      this.drawSegments();
+    this.clearSegments();
+    this.drawSegments();
   }
 }
--- web/src/app/track.window.ts
+++ web/src/app/track.window.ts
@@ -1,11 +1,10 @@
 import { CanvasPoint } from './canvas.point';
-import { Input } from '@angular/core';
 
 export class TrackWindow{
   canvasWidth = 500;
   canvasHeight = 500;
   center = new CanvasPoint(-52.218495, -31.773908);//lon, lat - x, y
-  @Input() lonZoom = 0.0002;
+  lonZoom = 0.0002;
 
   getLatZoom():number{
     return this.lonZoom * (this.canvasHeight / this.canvasWidth);
