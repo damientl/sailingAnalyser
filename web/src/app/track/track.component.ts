2207178cadf13da5a6028dd01bdddc65ba8d2a1b
damientl <damien.tl@gmail.com>
2017-10-26 11:49:42 -0200

    segment drawing

--- web/src/app/track/track.component.ts
+++ web/src/app/track/track.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { SegmentComponent } from '../segment/segment.component';
-import { SegmentService } from '../service/segment.service';
 import { Segment } from '../model/segment';
 import { TrackWindow } from '../model/track.window';
-import { CanvasPoint } from '../model/canvas.point';
 import { SpeedMath } from '../util/speed.math';
 import { CanvasMath } from '../util/canvas.math';
-import { DateUtil } from '../util/date.util';
 import { Borders } from '../model/borders';
+import { SegmentService } from '../service/segment.service';
+import { CanvasPoint } from '../model/canvas.point';
+import { DateUtil } from '../util/date.util';
+import { SegmentDrawing } from '../util/segment.drawing';
 
 @Component({
   selector: 'track',
@@ -21,49 +22,23 @@ export class TrackComponent implements OnInit  {
 
   segments: Segment[];
   trackWindow: TrackWindow;
+  segmentDrawing:SegmentDrawing;
 
   constructor(private segmentService: SegmentService) {
     this.trackWindow = new TrackWindow();
     this.segments = [];
   }
-
-  drawSegments(): void {
-    if (this.segments.length === 0) {
-      return;
-    }
-
-    let i = 1;
-    for (const seg of this.segments) {
-      this.drawSegment(seg, i);
-      i++;
-    }
-  }
-
-  drawSegment(seg: Segment, i): void {
-    if (i >= this.segments.length) {
-      return;
-    }
-
-    const canvasMath = new CanvasMath();
-    const nextSeg: Segment = this.segments[i];
-    const speedMath = new SpeedMath();
-
-    const color = speedMath.speedColor(seg, nextSeg);
-
-    this.segmentComponent.drawSegLine(canvasMath.pointOnCanvas(seg.segToPoint(), this.trackWindow),
-                    canvasMath.pointOnCanvas(nextSeg.segToPoint(), this.trackWindow)
-                    , color);
-  }
   getSegments(): void {
     this.segmentService.getSegmentsRest().then(
         (val) => {
           this.segments = val;
-          this.setupCanvas();
-          this.drawSegments();
+          this.segmentDrawing = new SegmentDrawing(this.segmentComponent, this.segments);
+          this.segmentDrawing.drawSegments();
         },
         (err) => console.error(err)
       );
   }
+
 
   ngOnInit(): void {
     this.segmentComponent.setTrackWindow(this.trackWindow);
@@ -73,17 +48,11 @@ export class TrackComponent implements OnInit  {
   handleZoomChange(event: number): void {
     this.trackWindow.setZoom(event);
     this.segmentComponent.clearSegments();
-    this.drawSegments();
+    this.segmentDrawing.drawSegments();
   }
 
   handleTimeChange(event: number): void {
     console.log('time');
   }
 
-  setupCanvas():void{
-    const canvasMath = new CanvasMath();
-    const borders:Borders = canvasMath.findBorders(this.segments);
-    this.trackWindow.setIniZoom(canvasMath.findBiggestDistanceSegments(borders));
-    this.trackWindow.setCenter(canvasMath.findCenterPoint(borders));
-  }
 }
---
+++ web/src/app/util/segment.drawing.ts
@@ -0,0 +1,51 @@
+
+import { SegmentComponent } from '../segment/segment.component';
+import { Segment } from '../model/segment';
+import { TrackWindow } from '../model/track.window';
+import { SpeedMath } from '../util/speed.math';
+import { CanvasMath } from '../util/canvas.math';
+import { Borders } from '../model/borders';
+
+export class SegmentDrawing{
+
+    trackWindow: TrackWindow;
+
+    constructor(private segmentComponent:SegmentComponent, private segments:Segment[]) {
+      this.trackWindow = segmentComponent.trackWindow;
+      this.setupCanvas();
+    }
+
+    drawSegments(): void {
+      if (this.segments.length === 0) {
+        return;
+      }
+
+      let i = 1;
+      for (const seg of this.segments) {
+        this.drawSegment(seg, i);
+        i++;
+      }
+    }
+
+    drawSegment(seg: Segment, i): void {
+      if (i >= this.segments.length) {
+        return;
+      }
+
+      const canvasMath = new CanvasMath();
+      const nextSeg: Segment = this.segments[i];
+      const speedMath = new SpeedMath();
+
+      const color = speedMath.speedColor(seg, nextSeg);
+
+      this.segmentComponent.drawSegLine(canvasMath.pointOnCanvas(seg.segToPoint(), this.trackWindow),
+                      canvasMath.pointOnCanvas(nextSeg.segToPoint(), this.trackWindow)
+                      , color);
+    }
+    setupCanvas():void{
+      const canvasMath = new CanvasMath();
+      const borders:Borders = canvasMath.findBorders(this.segments);
+      this.trackWindow.setIniZoom(canvasMath.findBiggestDistanceSegments(borders));
+      this.trackWindow.setCenter(canvasMath.findCenterPoint(borders));
+    }
+}
