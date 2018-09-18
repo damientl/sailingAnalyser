0b5bf6a2d2f402082c3c803d9f70dee0d2484bba
damientl <damien.tl@gmail.com>
2018-09-17 23:10:42 -0300

    forward and back play

--- web/src/app/time/time.component.ts
+++ web/src/app/time/time.component.ts
@@ -1,4 +1,5 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { Observable, Subscription} from 'rxjs';
 
 @Component({
   selector: 'time',
@@ -21,6 +22,10 @@ export class TimeComponent {
     @Output()
     changeValue: EventEmitter<number> = new EventEmitter<number>();
 
+    startStop = false;
+    subscription: Subscription;
+    timer;
+    delta;
 
     constructor() {
       this.minValue = 0;
@@ -35,9 +40,41 @@ export class TimeComponent {
       this.circleSize = 8;
       this.toggleDisable = 'enable';
       this.toggleDisableText = 'disable';
+
+      this.timer = Observable.timer(10, 200);
+
    }
+
+   subscribeTimer(){
+     this.subscription = this.timer.subscribe(t => {
+           this.updateValue();
+           this.changeValue.emit(this.value);
+     });
+   }
+
+   updateValue() {
+        this.value = this.value + this.delta/4;
+        if(this.value < 0) {
+          this.value = 0;
+        }
+        if(this.value > 100){
+          this.value = 100;
+        }
+   }
+
    selectedValue(iSelectedValue) {
       this.value = iSelectedValue;
       this.changeValue.emit(iSelectedValue);
+   }
+   onTimeChange(delta: number) {
+     this.delta = delta;
+
+     this.startStop = !this.startStop;
+
+     if(!this.startStop){
+       this.subscription.unsubscribe();
+     }else{
+       this.subscribeTimer();
+     }
    }
 }
